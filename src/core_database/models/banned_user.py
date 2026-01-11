bbd0e226ff2f5a9348f4b95b136e0db5949c9d80
dima <vds788502@gmail.com>
2026-01-11 17:49:22 +0400

    добавлена модель таблицы для забаненных юзеров

---
+++ src/core_database/models/banned_user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from src.core_database.models.base import Base
+
+
+class BannedUser(Base):
+    __tablename__ = "banned_user"
+    id_user: Mapped[int]
+    id_channel: Mapped[int]
+
+    def __str__(self):
+        return f"{self.__class__.__name__} (id_user={self.id_user}, id_channel={self.id_channel})"
+
+    def __repr__(self):
+        return str(self)
