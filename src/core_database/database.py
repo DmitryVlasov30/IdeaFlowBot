2235f23d9424518f9a68b97fddb4ff69f5b0ff93
dima <vds788502@gmail.com>
2026-01-11 19:13:28 +0400

    добавлены функции для взаимодействия с бд

--- src/core_database/database.py
+++ src/core_database/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import insert, select, and_, delete, update
 
 from src.core_database.models.base import Base
 from src.core_database.models.banned_user import BannedUser
+from src.core_database.models.channel_suggest import ChannelSuggest
+from src.core_database.models.chat_admins import ChatAdmins
 from src.core_database.models.db_helper import db_helper
 
 
@@ -41,3 +43,67 @@ class CrudBannedUser:
             )
             conn.execute(stmt)
             conn.commit()
+
+
+class CrudChannelSuggest:
+    @staticmethod
+    def get_chat_suggest():
+        with db_helper.engine.connect() as conn:
+            return conn.execute(select(ChannelSuggest)).fetchall()
+
+    @staticmethod
+    def add_chat_suggest(data: dict):
+        with db_helper.engine.connect() as conn:
+            conn.execute(insert(ChannelSuggest).values(data))
+            conn.commit()
+
+    @staticmethod
+    def delete_chat_suggest(data: dict):
+        with db_helper.engine.connect() as conn:
+            conn.execute(
+                delete(ChannelSuggest)
+                .filter(and_(
+                    ChannelSuggest.channel_id == data["channel_id"],
+                    ChannelSuggest.bot_id == data["bot_id"],
+                ))
+            )
+            conn.commit()
+
+
+class CrudChatAdmins:
+    @staticmethod
+    def get_chat_admins(bot: int = None, chat: int = None):
+        with db_helper.engine.connect() as conn:
+            if bot is None and chat is None:
+                return conn.execute(select(ChatAdmins)).fetchall()
+            elif bot is None:
+                return conn.execute(select(ChatAdmins).filter(ChatAdmins.chat_id == chat)).fetchall()
+            elif chat is None:
+                return conn.execute(select(ChatAdmins).filter(ChatAdmins.bot_id == bot)).fetchall()
+            else:
+                return conn.execute(
+                    select(ChatAdmins)
+                    .filter(and_(
+                        ChatAdmins.chat_id == chat,
+                        ChatAdmins.bot_id == bot
+                    ))
+                ).fetchall()
+
+    @staticmethod
+    def add_chat_admins(data: dict):
+        with db_helper.engine.connect() as conn:
+            conn.execute(insert(ChatAdmins).values(data))
+            conn.commit()
+
+    @staticmethod
+    def delete_chat_admins(data: dict):
+        with db_helper.engine.connect() as conn:
+            conn.execute(
+                delete(ChatAdmins)
+                .filter(and_(
+                    ChatAdmins.chat_id == data["chat_id"],
+                    ChatAdmins.bot_id == data["bot_id"],
+                ))
+            )
+            conn.commit()
+
