53199a5a1c5fb74c234d8bc8b94c6abf78438837
dima <vds788502@gmail.com>
2026-01-11 19:13:04 +0400

    таблица чатов, в которых тот или иной бот администратор

---
+++ src/core_database/models/chat_admins.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Mapped
+from src.core_database.models.base import Base
+from typing import Optional
+
+
+class ChatAdmins(Base):
+    __tablename__ = 'chat_admins'
+    bot_id: Mapped[int]
+    chat_id: Mapped[int]
+
+    def __str__(self):
+        return f"{self.__class__.__name__} (bot_id={self.bot_id}, chat_id={self.chat_id})"
+
+    def __repr__(self):
+        return str(self)
+
