e48cbcc8e3ac0e1db0bf89979ad34df25d5b4584
dima <vds788502@gmail.com>
2026-01-12 16:49:11 +0400

    добавлен столбец с id бота

--- src/core_database/models/banned_user.py
+++ src/core_database/models/banned_user.py
@@ -6,6 +6,7 @@ class BannedUser(Base):
     __tablename__ = "banned_user"
     id_user: Mapped[int]
     id_channel: Mapped[int]
+    bot_id: Mapped[int]
 
     def __str__(self):
         return f"{self.__class__.__name__} (id_user={self.id_user}, id_channel={self.id_channel})"
