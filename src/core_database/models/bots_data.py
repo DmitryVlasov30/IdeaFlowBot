76dc90cde2253e86eb2528495dbd8bb63c79b3aa
dima <vds788502@gmail.com>
2026-01-12 18:30:06 +0400

    добавлен столбец bot_username

--- src/core_database/models/bots_data.py
+++ src/core_database/models/bots_data.py
@@ -5,6 +5,7 @@ from src.core_database.models.base import Base
 class BotsData(Base):
     __tablename__ = "bots_data"
     bot_api_token: Mapped[str]
+    bot_username: Mapped[str]
     channel_id: Mapped[int]
 
     def __str__(self):
