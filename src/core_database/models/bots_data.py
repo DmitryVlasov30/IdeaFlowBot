80698c5e298986251886268bbc2d27545cd60f01
dima <vds788502@gmail.com>
2026-01-12 16:46:31 +0400

    таблица для подчиненных ботов

--- src/core_database/models/bots_data.py
+++ src/core_database/models/bots_data.py
@@ -2,13 +2,13 @@ from sqlalchemy.orm import Mapped
 from src.core_database.models.base import Base
 
 
-class ChannelSuggest(Base):
-    __tablename__ = "chat_suggest"
-    bot_id: Mapped[int]
+class BotsData(Base):
+    __tablename__ = "bots_data"
+    bot_api_token: Mapped[str]
     channel_id: Mapped[int]
 
     def __str__(self):
-        return f"{self.__class__.__name__} (bot_id={self.bot_id}, channel_id={self.channel_id})"
+        return f"{self.__class__.__name__} (channel_id={self.channel_id})"
 
     def __repr__(self):
         return str(self)
