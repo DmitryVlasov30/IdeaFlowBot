0efc6042e2eb7c92f50ea783963d80e8bb594fe5
dima <vds788502@gmail.com>
2026-03-24 15:55:34 +0400

    перенесены функции: get_timestamp_public, save_post

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
-from telebot.types import Message
+from telebot.types import Message, CallbackQuery
+from datetime import datetime, timedelta, timezone
+from loguru import logger
 
 from src.core_database.database import CrudBannedUser
 from config import settings
@@ -10,6 +12,27 @@ class Utils:
         db_session = CrudBannedUser()
         all_info = await db_session.get_banned_users(id_user=id_user, id_channel=id_channel)
         return bool(all_info)
+
+    @staticmethod
+    async def get_timestamp_public(time) -> float:
+        tz = timezone(timedelta(hours=3))
+        hour, minute = map(int, time.split(':'))
+        now = datetime.now(tz)
+        target = now.replace(hour=hour, minute=minute)
+        if now >= target:
+            target += timedelta(days=1)
+
+        time_public = target.timestamp()
+        return time_public
+
+    @staticmethod
+    @logger.catch
+    async def save_post(call: CallbackQuery, public_posts, channel_id):
+        if call.message.content_type == "text":
+            await public_posts.add_public_posts({
+                "channel_id": channel_id,
+                "posts_title": call.message.text,
+            })
 
 
 def filter_chats(func):
