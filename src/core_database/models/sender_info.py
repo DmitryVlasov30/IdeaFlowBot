c9c6cb97a912e3ef96e9419d64fc48c33ba1b8f8
dima <vds788502@gmail.com>
2026-04-06 03:11:37 +0400

    обновленная таблицы с отправителями постов

---
+++ src/core_database/models/sender_info.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped
+from src.core_database.models.base import Base
+
+
+class SenderData(Base):
+    __tablename__ = "users"
+
+    user_id: Mapped[int]
+    channel_id: Mapped[str]
+    bot_username: Mapped[str]
+    username: Mapped[str]
+    first_name: Mapped[str]
+    message_id: Mapped[int]
+    chat_id: Mapped[int]
+    text_post: Mapped[str]
